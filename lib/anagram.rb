cdba59a1867c38b82f55235bfaf8cd40cdd70cad
Samantha Barros <samantha.barros95@gmail.com>
2020-06-16 04:38:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
     new_array = []
     if new_array.include?(words)
       new_array << words
-    elsif new_array.reject?
+    elsif new_array.reject!
       new_array.reverse
      end 
   end 
