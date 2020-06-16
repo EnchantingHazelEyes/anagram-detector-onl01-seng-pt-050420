6943f714fdb2f045d7c291459a896716a753de3f
Samantha Barros <samantha.barros95@gmail.com>
2020-06-16 04:22:23 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,7 @@ class Anagram
     new_array = []
       if new_array.include?(words)
         new_array << words
-      elsif
-        new_array.reverse_each {|words| str += "#{words} "}
+      elsif new_array.reverse_each {|words| str += "#{words} "}
         words
    end 
   end 
